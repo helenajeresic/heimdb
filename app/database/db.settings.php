1bc052c90c08506d17c3b1a60bb37f3ec10ab3d1
helenajeresic <helenajeresic12@gmail.com>
2023-06-23 14:49:52 +0200

    database updateded

--- app/database/db.settings.php
+++ app/database/db.settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$db_base = 'mysql:host=localhost;dbname=krizanec;s+charset=utf8';
+$db_base = 'mysql:host=localhost;dbname=jeresic;s+charset=utf8';
 $db_user = 'student';
 $db_pass = 'pass.mysql';
 
