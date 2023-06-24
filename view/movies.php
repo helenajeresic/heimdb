098763e431682cda7a5eec8084ce3cfbca5debd2
ikrizanec <ivanakrizanec@hotmail.com>
2023-06-24 16:36:55 +0200

    movie and wishlist view

--- view/movies.php
+++ view/movies.php
@@ -1,17 +1,24 @@
 <?php require_once __SITE_PATH . '/view/_header.php';?>
 
-<?php foreach( $show_movies as $index => $m ) { 
-    $src = "https://heimdb.s3.eu-north-1.amazonaws.com/" . $m->__get( 'image' );?>
-        <div>
-            <img src="<?php echo $src;?>" class="movie_image" alt="<?php echo $m->__get( 'title' );?>" 
-            sytle="width: 200px; height: auto;">
-            <div class="title"><?php echo $m->__get( 'title' );?></div>
+<div class="container">
+    <div class="movie-container">
+    <div class="movie-content">
+            <?php foreach( $show_movies as $index => $m ) { 
+            $src = "https://heimdb.s3.eu-north-1.amazonaws.com/" . $m->__get( 'image' );?>
+
+            <div class="movie-box">
+                <img src="<?php echo $src;?>" class="movie-image" alt="<?php echo $m->__get( 'title' );?>" >
+                <div class="movie-data">
+                    <div class="movie-title"><?php echo $m->__get( 'title' );?></div>
+                    <div><?php echo $m->__get('year'); ?></div>
+                    <div><?php echo $m->__get('genre'); ?></div>
+                    <div><?php echo $m->__get('description'); ?></div>
+                    <div><?php echo $m->__get('duration'); ?></div>
+                </div>
+            </div>
+            <?php } ?>
         </div>
-    <?php echo $m->__get('year'); ?><br>
-    <?php echo $m->__get('genre'); ?><br>
-    <?php echo $m->__get('description'); ?><br>
-    <?php echo $m->__get('duration'); ?><br>
- 
-<?php } ?>
+    </div>
+</div>
 
 <?php require_once __SITE_PATH . '/view/_footer.php';?>
