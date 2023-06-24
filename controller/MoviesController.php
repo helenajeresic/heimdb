837048a26a9df2c33685d99029389eb5ea66414e
ikrizanec <ivanakrizanec@hotmail.com>
2023-06-24 14:00:47 +0200

    update

--- controller/MoviesController.php
+++ controller/MoviesController.php
@@ -191,5 +191,27 @@ class moviesController extends BaseController {
         }  
     }
 
+    public function addMovie()
+    {
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
+            $this->registry->template->error = false;
+            $this->registry->template->show('login');
+        }
+        else {
+            $ms = new MovieService();
+            if( isset($_POST['id_movie']) && isset($_POST['title']) && isset($_POST['year']) &&
+                isset($_POST['genre']) && isset($_POST['description']) && isset($_POST['image']) && isset($_POST['duration'])){
+                $data = $ms->getMostPopular();
+
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            } 
+            else {
+
+            }
+        }  
+    }
+
 }
 ?>
