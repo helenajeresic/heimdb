f883e9f87f67a1fb11d1553d85b6ba3fc1cc936c
ikrizanec <ivanakrizanec@hotmail.com>
2023-06-24 12:44:20 +0200

    movies controller

--- controller/MoviesController.php
+++ controller/MoviesController.php
@@ -1,20 +1,149 @@
 <?php
 
 require_once __SITE_PATH . '/model/movies_service.class.php';
+require_once __SITE_PATH . '/model/watchlist_service.class.php';
 
 class moviesController extends BaseController {
     public function index() {
-        if(!isset($_SESSION["username"])) {
-            $this->registry->template->title = "Login";
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
             $this->registry->template->error = false;
-            $this->registry->template->show("login");
+            $this->registry->template->show('login');
         }
         else {
             $ms = new MovieService();
             $data = $ms->getAllMovies();
             $this->registry->template->show_movies = $data;
-            $this->registry->template->show("movies");
+            $this->registry->template->show('movies');
         }
     }
+
+    public function showUsersWatchlist()
+    {
+
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
+            $this->registry->template->error = false;
+            $this->registry->template->show('login');
+        }
+        else {
+            $ms = new WatclistService();
+            $data = $ms->getUsersWatchlistById( $_SESSION['id_user'] );
+            $this->registry->template->show_movies = $data;
+            $this->registry->template->show('movies');
+        }       
+    }
+
+    public function showUsersWatched()
+    {
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
+            $this->registry->template->error = false;
+            $this->registry->template->show('login');
+        }
+        else {
+            $ms = new WatclistService();
+            $data = $ms->getWatchedMoviesById( $_SESSION['id_user'] );
+            $this->registry->template->show_movies = $data;
+            $this->registry->template->show('movies');
+        }    
+    }
+
+    public function addToWatchlist()
+    {
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
+            $this->registry->template->error = false;
+            $this->registry->template->show('login');
+        }
+        else {
+            $ms = new WatclistService();
+            if(isset($_POST['id_movie'])) {
+                $ms->addMovieToWatchlist( $_SESSION['id_user'], $_POST['id_movie'] );
+                $data = $ms->getUsersWatchlistById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            } 
+            else {
+                $ms = new WatclistService();
+                $data = $ms->getWatchedMoviesById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            }
+        }  
+    }
+
+    public function removeFromWatchlist()
+    {
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
+            $this->registry->template->error = false;
+            $this->registry->template->show('login');
+        }
+        else {
+            $ms = new WatclistService();
+            if(isset($_POST['id_movie'])) {
+                $ms->removeMovieFromWatchlist( $_SESSION['id_user'], $_POST['id_movie'] );
+                $data = $ms->getUsersWatchlistById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            } 
+            else {
+                $ms = new WatclistService();
+                $data = $ms->getWatchedMoviesById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            }
+        }  
+    }
+
+    public function addToWatched()
+    {
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
+            $this->registry->template->error = false;
+            $this->registry->template->show('login');
+        }
+        else {
+            $ms = new WatclistService();
+            if(isset($_POST['id_movie'])) {
+                $ms->addWatchedMovie( $_SESSION['id_user'], $_POST['id_movie'] );
+                $data = $ms->getUsersWatchlistById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            } 
+            else {
+                $ms = new WatclistService();
+                $data = $ms->getWatchedMoviesById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            }
+        }  
+    }
+
+    public function removeFromWatched()
+    {
+        if(!isset($_SESSION['username'])) {
+            $this->registry->template->title = 'Login';
+            $this->registry->template->error = false;
+            $this->registry->template->show('login');
+        }
+        else {
+            $ms = new WatclistService();
+            if(isset($_POST['id_movie'])) {
+                $ms->removeWatchedMovie( $_SESSION['id_user'], $_POST['id_movie'] );
+                $data = $ms->getUsersWatchlistById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            } 
+            else {
+                $ms = new WatclistService();
+                $data = $ms->getWatchedMoviesById( $_SESSION['id_user'] );
+                $this->registry->template->show_movies = $data;
+                $this->registry->template->show('movies');
+            }
+        }  
+    }
+
 }
 ?>
