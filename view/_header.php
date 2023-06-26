5436271d74c64ffec4a5cbd7266faeb5bd74ce2b
helenajeresic <helenajeresic12@gmail.com>
2023-06-26 23:18:54 +0200

    nav bar improvments

--- view/_header.php
+++ view/_header.php
@@ -10,48 +10,47 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
-    <div class = "fixed">
-        <div style="display: inline-block;">
-            <img src=<?php echo __SITE_URL . '/images/heimdb.png';?> class = "icon_heimdb" alt="heimdb">
+<div class="fixed">
+  <div class="topnav">
+    <div class="left-section">
+      <img src="<?php echo __SITE_URL . '/images/heimdb.png';?>" class="icon_heimdb" alt="heimdb">
+      <?php if (isset($_SESSION['username'])) { ?>
+      <div class="search-container">
+        <form action="/action_page.php">
+          <input type="text" placeholder="Search movies" name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div>
+      <?php } ?>
+    </div>
+    <div class="right-section">
+      <?php if (isset($_SESSION['username'])) { ?>
+      <a href="index.php?rt=movies"><i class="fas fa-home"></i>Home</a>
+      <div class="dropdown">
+        <button class="dropbtn active"><i class="fas fa-bars"></i>Menu</button>
+        <div class="dropdown-content">
+          <a href="index.php?rt=movies/topRated">Top rated</a>
+          <a href="index.php?rt=movies/mostWatched">Most watched</a>
+          <a href="index.php?rt=movies/mostPopular">Most popular</a>
         </div>
-        <?php if( isset ($_SESSION['username'] ) )
-        {?>
-
-            <div class="topnav">
-                <div class="search-container">
-                    <form action="/action_page.php">
-                      <input type="text" placeholder="Search movies" name="search">
-                      <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                  </div>
-                <a href="index.php?rt=movies"><i class="fas fa-home"></i>Home</a>
-                <div class="dropdown">
-                    <button class="dropbtn active"><i class="fas fa-bars"></i>Menu
-                    </button>
-                    <div class="dropdown-content">
-                      <a href="index.php?rt=movies/topRated">Top rated</a>
-                      <a href="index.php?rt=movies/mostWatched">Most watched</a>
-                      <a href="index.php?rt=movies/mostPopular">Most popular</a>
-                    </div>
-                  </div>
-                <a href="index.php?rt=watchlist"><i class="fas fa-film"></i>Watchlist</a>
-                <div class="dropdown">
-                    <button class="dropbtn active"><i class="fas fa-user"></i><?php echo $_SESSION['username']; ?>
-                    </button>
-                    <div class="dropdown-content">
-                      <a href="index.php?rt=comments/myComments">My Comments</a>
-                      <a href="index.php?rt=rates/myRates">My Rates</a>
-                      <a href="index.php?rt=users/updateProfile">Update Profile</a>
-                      <?php if($_SESSION['admin'] == 1 )
-                      { ?>
-                          <a href="index.php?rt=users/deleteUser">Delete User</a>
-                          <a href="index.php?rt=movies/addMovie">Add Movie</a>
-                      <?php } ?>
-                    </div>
-                  </div>
-                <a href="index.php?rt=logout"><i class="fas fa-sign-out-alt"></i>Log out</a>
-            </div>
+      </div>
+      <a href="index.php?rt=watchlist"><i class="fas fa-film"></i>Watchlist</a>
+      <div class="dropdown">
+        <button class="dropbtn active"><i class="fas fa-user"></i><?php echo $_SESSION['username']; ?></button>
+        <div class="dropdown-content">
+          <a href="index.php?rt=comments/myComments">My Comments</a>
+          <a href="index.php?rt=rates/myRates">My Rates</a>
+          <a href="index.php?rt=users/updateProfile">Update Profile</a>
+          <?php if ($_SESSION['admin'] == 1) { ?>
+          <a href="index.php?rt=users/deleteUser">Delete User</a>
+          <a href="index.php?rt=movies/addMovie">Add Movie</a>
+          <?php } ?>
+        </div>
+      </div>
+      <a href="index.php?rt=logout"><i class="fas fa-sign-out-alt"></i>Log out</a>
+      <?php } ?>
+    </div>
+  </div>
+</div>
 
 
-       <?php } ?>
-   </div>
