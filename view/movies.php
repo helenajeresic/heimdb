0575f599bf51a4ae05bc5ab62f6b6c9c4c279882
ikrizanec <ivanakrizanec@hotmail.com>
2023-06-26 23:49:40 +0200

    update view

--- view/movies.php
+++ view/movies.php
@@ -2,18 +2,27 @@
 
 <div class="container">
     <div class="movie-container">
-    <div class="movie-content">
+        <h1>Top rated movies:</h1>
+        <div class="movie-content">
             <?php foreach( $show_movies as $index => $m ) { 
             $src = "https://heimdb.s3.eu-north-1.amazonaws.com/" . $m->__get( 'image' );?>
 
             <div class="movie-box">
                 <img src="<?php echo $src;?>" class="movie-image" alt="<?php echo $m->__get( 'title' );?>" >
                 <div class="movie-data">
-                    <div class="movie-title"><?php echo $m->__get( 'title' );?></div>
-                    <div><?php echo $m->__get('year'); ?></div>
-                    <div><?php echo $m->__get('genre'); ?></div>
-                    <div><?php echo $m->__get('description'); ?></div>
-                    <div><?php echo $m->__get('duration'); ?></div>
+                    <div class="movie-title">
+                        <h2><?php echo $m->__get( 'title' );?></h2>
+                    </div class>
+                    <div class = "movie-buttons">
+                        <button class="remove-watched-button" onclick="removeFromWatched(<?php echo $m->__get('id');?>)">&#x2764;</button>
+                        <button class="remove-watchlist-button" onclick="removeFromWatchlist(<?php echo $m->__get('id');?>)">&#x1F4FA;</button>
+                    </div>
+                    <div class="movie-atributes">
+                        <?php echo $m->__get('year'); ?> | 
+                        <?php echo $m->__get('duration'); ?> | 
+                        <?php echo $m->__get('genre'); ?>
+                    </div>
+                    <?php echo $m->__get('description'); ?><br>
                 </div>
             </div>
             <?php } ?>
--- view/watchlist.php
+++ view/watchlist.php
@@ -11,20 +11,18 @@
                 <img src="<?php echo $src;?>" class="movie-image" alt="<?php echo $m->__get( 'title' );?>" >
                 <div class="movie-data">
                     <div class="movie-title">
-                        <div class="movie_title">
-                            <h2><?php echo $m->__get( 'title' );?></h2>
-                        </div>
-                        <div class = "movie-buttons">
-                            <button class="remove-watched-button" onclick="removeFromWatched(<?php echo $m->__get('id');?>)">&#x2764;</button>
-                            <button class="remove-watchlist-button" onclick="removeFromWatchlist(<?php echo $m->__get('id');?>)">&#x1F4FA;</button>
-                        </div>
-                        <div class="atributes">
-                            <?php echo $m->__get('year'); ?> | 
-                            <?php echo $m->__get('duration'); ?> | 
-                            <?php echo $m->__get('genre'); ?>
-                        </div>
-                        <?php echo $m->__get('description'); ?><br>
+                        <h2><?php echo $m->__get( 'title' );?></h2>
+                    </div class>
+                    <div class = "movie-buttons">
+                        <button class="remove-watched-button" onclick="removeFromWatched(<?php echo $m->__get('id');?>)">&#x2764;</button>
+                        <button class="remove-watchlist-button" onclick="removeFromWatchlist(<?php echo $m->__get('id');?>)">&#x1F4FA;</button>
                     </div>
+                    <div class="movie-atributes">
+                        <?php echo $m->__get('year'); ?> | 
+                        <?php echo $m->__get('duration'); ?> | 
+                        <?php echo $m->__get('genre'); ?>
+                    </div>
+                    <?php echo $m->__get('description'); ?><br>
                 </div>
             </div>
             <?php } ?>
