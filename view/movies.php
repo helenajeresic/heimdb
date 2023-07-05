e1f5e59521e4549b84f08fc7100c7d1624509662
ikrizanec <ivanakrizanec@hotmail.com>
2023-07-05 19:43:06 +0200

    view update

--- view/movies.php
+++ view/movies.php
@@ -13,19 +13,19 @@
                     <option value="byRating">HEIMDB rating</option>
                 </select>
             </div>
-        </div>
+        </div >
         <div class="movie-content">
             <?php foreach( $show_movies as $index => $m ) { 
-            $src = "https://heimdb.s3.eu-north-1.amazonaws.com/" . $m->__get( 'image' );?>
-
+            $src = "https://heimdb.s3.eu-north-1.amazonaws.com/" . $m->__get( 'image' );
+            $mov = 'http://' . $_SERVER['SERVER_NAME'] . htmlentities( dirname( $_SERVER['PHP_SELF'] ) ) . '/index.php?rt=Movies/showMovie&id_movie=' . $m->__get( 'id_movie' );?>
             <div class="movie-box">
                 <img src="<?php echo $src;?>" class="movie-image" alt="<?php echo $m->__get( 'title' );?>" >
                 <div class="movie-data">
-                    <div class="movie-title">
-                        <h2><?php echo $m->__get( 'title' );?></h2>
-                    </div class>
+                <div class="movie-title">
+                    <a href="<?php echo $mov;?>"><?php echo $m->__get( 'title' );?></a>
+                </div>
                     <div class = "movie-buttons">
-                        <p>Rating: </p>
+                        <button class="rating-button">&#9733; 3.5</button>
                         <button class="remove-watched-button" onclick="removeFromWatched(<?php echo $m->__get('id');?>)">&#x2764;</button>
                         <button class="remove-watchlist-button" onclick="removeFromWatchlist(<?php echo $m->__get('id');?>)">&#x1F4FA;</button>
                     </div>
