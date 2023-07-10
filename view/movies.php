f1f8251ad8c4c7401eb396fac7fa04b078de99a3
helenajeresic <helenajeresic12@gmail.com>
2023-07-10 02:21:10 +0200

    removed confirm

--- view/movies.php
+++ view/movies.php
@@ -39,12 +39,10 @@
                         </div>
                         <div class="movie-buttons">
                             <button class="rating-button" onclick="showRatingPopup('<?php echo $popupId; ?>')">&#9733; <?php echo $ratings[$m->__get('id_movie')]; ?></button>
-                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatchlist' ;?>" 
-                                onsubmit="return confirm('Are you sure you want to update this movie on the watchlist?');" id="watchlistForm">
+                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatchlist' ;?>" id="watchlistForm">
                                 <button type="submit" class="remove-watched-button" name="id_movie" value="<?php echo $m->__get('id_movie'); ?>">&#x2764;</button>
                              </form>
-                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatched'; ?>" 
-                                onsubmit="return confirm('Are you sure you want to update this movie on the watchlist?');" id="watchedForm">
+                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatched'; ?>" id="watchedForm">
                                 <button type="submit" class="remove-watched-button" name="id_movie" value="<?php echo $m->__get('id_movie'); ?>">&#x1F4FA;</button>
                             </form>
                         </div>
--- view/watchlist.php
+++ view/watchlist.php
@@ -21,12 +21,10 @@
                 </div>
                     <div class = "movie-buttons">
                     <button class="rating-button" onclick="showRatingPopup('<?php echo $popupId; ?>')">&#9733; <?php echo $ratings_watchlist[$m->__get('id_movie')]; ?></button>
-                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatchlist' ;?>" 
-                                onsubmit="return confirm('Are you sure you want to update this movie on the watchlist?');" id="watchlistForm1">
+                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatchlist' ;?>"  id="watchlistForm1">
                                 <button type="submit" class="remove-watchlist-button" name="id_movie" value="<?php echo $m->__get('id_movie'); ?>">&#x2764;</button>
                              </form>
-                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatched'; ?>" 
-                                onsubmit="return confirm('Are you sure you want to update this movie on the watchlist?');" id="watchedForm1">
+                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatched'; ?>" id="watchedForm1">
                                 <button type="submit" class="remove-watched-button" name="id_movie" value="<?php echo $m->__get('id_movie'); ?>">&#x1F4FA;</button>
                             </form>
                     </div>
@@ -105,12 +103,10 @@
                 </div>
                     <div class = "movie-buttons">
                     <button class="rating-button" onclick="showRatingPopup('<?php echo $popupId; ?>')">&#9733; <?php echo $ratings_watched[$m->__get('id_movie')]; ?></button>
-                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatchlist' ;?>" 
-                                onsubmit="return confirm('Are you sure you want to update this movie on the watchlist?');" id="watchlistForm2">
+                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatchlist' ;?>"  id="watchlistForm2">
                                 <button type="submit" class="remove-watched-button" name="id_movie" value="<?php echo $m->__get('id_movie'); ?>" disabled >&#x2764;</button>
                              </form>
-                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatched'; ?>" 
-                                onsubmit="return confirm('Are you sure you want to update this movie on the watchlist?');" id="watchedForm2">
+                            <form method="post" action="<?php echo __SITE_URL . '/index.php?rt=watchlist/updateWatched'; ?>"  id="watchedForm2">
                                 <button type="submit" class="remove-watched-button" name="id_movie" value="<?php echo $m->__get('id_movie'); ?>">&#x1F4FA;</button>
                             </form>
                     </div>
